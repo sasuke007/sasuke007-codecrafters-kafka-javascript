6da1c24956f2583cd43dd0a740cedb95258866dd
= <pandit.rohit0007@gmail.com>
2024-12-01 13:59:30 +0530

    Initial Commit.

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 import net from "net";
+import something from "./something.js";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+    something(connection);
 });
 
 server.listen(9092, "127.0.0.1");
