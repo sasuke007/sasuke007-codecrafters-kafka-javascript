019cd570b1c2b5252a1c335344ae1431e9793ac5
= <pandit.rohit0007@gmail.com>
2024-12-01 18:49:51 +0530

    Reading correlation id.

--- app/main.js
+++ app/main.js
@@ -5,10 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-    let arr = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 7]);
+
     connection.on("data", (data ) => {
         // console.log(data.subarray(8, 13));
-        connection.write(data.subarray(8, 12));
+        connection.write(new Uint8Array([0, 0, 0, 0, data[8], data[9], data[10], data[11]]));
     });
 });
 
