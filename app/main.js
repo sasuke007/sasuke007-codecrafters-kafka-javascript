01f37492a032443d2313c132a3d19f8dca5a97d4
= <pandit.rohit0007@gmail.com>
2024-12-01 18:40:26 +0530

    Reading correlation id.

--- app/main.js
+++ app/main.js
@@ -1,12 +1,15 @@
 import net from "net";
-import something from "./something.js";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-    something(connection);
+    let arr = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 7]);
+    connection.on("data", (data ) => {
+        // console.log(data.subarray(8, 13));
+        connection.write(data.subarray(8, 13));
+    });
 });
 
 server.listen(9092, "127.0.0.1");
